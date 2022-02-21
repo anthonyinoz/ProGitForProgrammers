00aa2900329ca0e7b537d41e55ed6ce2094f8ecc
anthonyinoz <tony.loriente@gmail.com>
2022-02-21 11:32:32 +1100

    Rewrite of program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-public class Program
+class Program
 {
-    public void PrintHello()
+    static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        Console.WriteLine("I just added this in Visual Studio");
+        Console.WriteLine("I just added this in the command line repo");
     }
 }
 
